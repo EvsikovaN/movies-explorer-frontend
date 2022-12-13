e6a12538c56f7a195b833c9ee30c2dea1c51b181
EvsikovaN <evsikova32@gmail.com>
2022-12-13 19:53:34 +0300

    fixes

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,29 +1,16 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./NotFoundPage.css";
-import { useState } from "react";
-import { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
 
 function NotFoundPage({ loggedIn }) {
   // const navigate = useNavigate();
-
-  const { pathname } = useLocation();
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    setCount(count+1);
-    console.log(pathname);
-  }, [pathname])
 
   return (
     <main>
       <div className="error">
         <h2 className="error__title">404</h2>
         <p className="error__description">Страница не найдена</p>
-        <Link className="error__link" to={loggedIn ? -(count) -2 : -(count)}>
-          Назад
-        </Link>
-        {/* {loggedIn ? (
+        {loggedIn ? (
           <Link className="error__link" to={-2}>
             Назад
           </Link>
@@ -31,7 +18,7 @@ function NotFoundPage({ loggedIn }) {
           <Link className="error__link" to={-1}>
             Назад
           </Link>
-        )} */}
+        )}
         {/* <Link className="error__link" to={-1}>
           Назад
         </Link> */}
